fb4bda8fbc716dad54bf289f991d7baed2431a0e
maxcutlyp <max@maxcutlyp.com>
2021-12-02 20:19:45 +0800

    add boilerplate textbox.py

---
+++ textbox.py
@@ -0,0 +1,11 @@
+import curses
+
+class TextBox:
+    def __init__(self, length: int, y: int, x: int):
+        pass
+
+    def draw(self, win):
+        pass
+
+    def handle_keypress(key):
+        pass
